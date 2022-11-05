3e1ca8faf8592cd138074d2da2f30676b9b91762
chiaraintech <chiaraintech@gmail.com>
2022-11-05 15:48:19 +0000

    retr

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavbarComponent from "./components/Navbar";
+
+function App() {
+  return (
+    <NavbarComponent>
+      
+    </NavbarComponent>
+  );
+}
+
+export default App;
