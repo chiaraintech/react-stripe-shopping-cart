e485c93aa0cbdd5e534e88f396f8c47077a2701b
chiaraintech <chiaraintech@gmail.com>
2022-11-06 10:36:40 +0000

    Made modal and on click it shows, then it hides

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
-import {Button, Container, Navbar, Modal} from 'react-bootstrap'
+import {Button, Container, Navbar, Modal} from 'react-bootstrap';
+import {useState} from "react";
 
 function NavbarComponent() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
+        <>
         <Navbar expand="sm">
             <Navbar.Brand href="/">Ecommerce shop</Navbar.Brand>
             <Navbar.Toggle/>
             <Navbar.Collapse className="justify-content-end">
-                <Button>Cart 0 items</Button>
+                <Button onClick={handleShow}>Cart 0 items</Button>
             </Navbar.Collapse>
         </Navbar>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+                <Modal.Title>
+                    <Modal.Body>
+                        This is the modal body.
+                    </Modal.Body>
+                </Modal.Title>
+            </Modal.Header>
+        </Modal>
+    </>
     )
 }
 
